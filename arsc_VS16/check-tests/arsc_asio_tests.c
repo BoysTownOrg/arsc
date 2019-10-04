e97392193b65fedfb4bb26eec84cadb7a66d0bb0
Bashford, Seth <seth.bashford@boystown.org>
2019-10-04 15:39:21 -0500

    refactor

--- arsc_VS16/check-tests/arsc_asio_tests.c
+++ arsc_VS16/check-tests/arsc_asio_tests.c
@@ -4,33 +4,44 @@
 
 static int32_t(*ar_asio_devices_restore)();
 
-static void setup(void) {
-	ar_asio_devices_restore = ar_asio_devices;
-}
-
-static void teardown(void) {
-	ar_asio_devices = ar_asio_devices_restore;
-}
-
 static int32_t devices;
 
 static int32_t ar_asio_devices_stub() {
 	return devices;
 }
 
+static void setup(void) {
+	ar_asio_devices_restore = ar_asio_devices;
+	ar_asio_devices = ar_asio_devices_stub;
+}
+
+static void teardown(void) {
+	ar_asio_devices = ar_asio_devices_restore;
+}
+
+static int32_t ar_asio_bind_() {
+	return _ar_asio_bind(0, 0);
+}
+
+static void set_devices(int32_t n) {
+	devices = n;
+}
+
+static void set_nonzero_devices() {
+	set_devices(1);
+}
+
 #define ASSERT_EQUAL_INT(a, b) ck_assert_int_eq(a, b)
 
 START_TEST(ar_asio_bind_returns_number_of_devices) {
-	ar_asio_devices = ar_asio_devices_stub;
-	devices = 3;
-	ASSERT_EQUAL_INT(3, _ar_asio_bind(0, 0));
+	set_devices(3);
+	ASSERT_EQUAL_INT(3, ar_asio_bind_());
 }
 END_TEST
 
-START_TEST(ar_asio_bind_assigns_devices_impl) {
-	ar_asio_devices = ar_asio_devices_stub;
-	devices = 3;
-	_ar_asio_bind(0, 0);
+START_TEST(ar_asio_bind_assigns_devices_impl_when_nonzero_devices) {
+	set_nonzero_devices();
+	ar_asio_bind_();
 	ck_assert(ar_asio_devices_stub == _ardvt[0].num_dev);
 }
 END_TEST
@@ -40,7 +51,7 @@ Suite* arsc_asio_test_suite() {
 	TCase* test_case = tcase_create("idk");
 	tcase_add_checked_fixture(test_case, setup, teardown);
 	tcase_add_test(test_case, ar_asio_bind_returns_number_of_devices);
-	tcase_add_test(test_case, ar_asio_bind_assigns_devices_impl);
+	tcase_add_test(test_case, ar_asio_bind_assigns_devices_impl_when_nonzero_devices);
 	suite_add_tcase(suite, test_case);
 	return suite;
 }
