b4ad9457a2e3b61e775da052f0fc18c4b017165d
Bashford, Seth <seth.bashford@boystown.org>
2019-10-04 15:46:49 -0500

    refactor

--- arsc_VS16/check-tests/arsc_asio_tests.c
+++ arsc_VS16/check-tests/arsc_asio_tests.c
@@ -32,26 +32,29 @@ static void set_nonzero_devices() {
 }
 
 #define ASSERT_EQUAL_INT(a, b) ck_assert_int_eq(a, b)
+#define ASSERT_EQUAL_ANY(a, b) ck_assert(a == b)
 
 START_TEST(ar_asio_bind_returns_number_of_devices) {
 	set_devices(3);
 	ASSERT_EQUAL_INT(3, ar_asio_bind_());
 }
-END_TEST
 
 START_TEST(ar_asio_bind_assigns_devices_impl_when_nonzero_devices) {
 	set_nonzero_devices();
 	ar_asio_bind_();
-	ck_assert(ar_asio_devices_stub == _ardvt[0].num_dev);
+	ASSERT_EQUAL_ANY(ar_asio_devices_stub, _ardvt[0].num_dev);
 }
-END_TEST
+
+static void add_test(TCase* test_case, const TTest* test) {
+	tcase_add_test(test_case, test);
+}
 
 Suite* arsc_asio_test_suite() {
 	Suite* suite = suite_create("arsc_asio");
 	TCase* test_case = tcase_create("idk");
 	tcase_add_checked_fixture(test_case, setup, teardown);
-	tcase_add_test(test_case, ar_asio_bind_returns_number_of_devices);
-	tcase_add_test(test_case, ar_asio_bind_assigns_devices_impl_when_nonzero_devices);
+	add_test(test_case, ar_asio_bind_returns_number_of_devices);
+	add_test(test_case, ar_asio_bind_assigns_devices_impl_when_nonzero_devices);
 	suite_add_tcase(suite, test_case);
 	return suite;
 }
