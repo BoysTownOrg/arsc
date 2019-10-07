eccf4c843c099785b114dc1676729b74f90a185f
Bashford, Seth <seth.bashford@boystown.org>
2019-10-07 09:33:34 -0500

    refactor

--- arsc_VS16/check-tests/arsc_asio_tests.c
+++ arsc_VS16/check-tests/arsc_asio_tests.c
@@ -32,7 +32,7 @@ static int32_t ar_asio_bind_with_device_type(int32_t device_type) {
 	return _ar_asio_bind(device_type, 0);
 }
 
-static int32_t ar_asio_bind_(void) {
+static int32_t bind(void) {
 	return ar_asio_bind_with_device_type(0);
 }
 
@@ -51,28 +51,31 @@ static int32_t(*bound_devices_impl(int32_t device_type))() {
 #define ASSERT_EQUAL_INT(a, b) ck_assert_int_eq(a, b)
 #define ASSERT_EQUAL_ANY(a, b) ck_assert(a == b)
 
-static void assert_asio_bind_assigns_devices_impl_when_nonzero_devices(int32_t device_type) {
+static void bind_nonzero_devices_with_device_type(int32_t device_type) {
 	set_nonzero_devices();
 	ar_asio_bind_with_device_type(device_type);
+}
+
+static void assert_bind_assigns_devices_impl_when_nonzero_devices(int32_t device_type) {
+	bind_nonzero_devices_with_device_type(device_type);
 	ASSERT_EQUAL_ANY(ar_asio_devices_stub, bound_devices_impl(device_type));
 }
 
-START_TEST(ar_asio_bind_returns_number_of_devices) {
+START_TEST(bind_returns_number_of_devices) {
 	set_devices(3);
-	ASSERT_EQUAL_INT(3, ar_asio_bind_());
+	ASSERT_EQUAL_INT(3, bind());
 }
 
-START_TEST(ar_asio_bind_assigns_devices_impl_to_device_type_zero_when_nonzero_devices) {
-	assert_asio_bind_assigns_devices_impl_when_nonzero_devices(0);
+START_TEST(bind_assigns_devices_impl_to_device_type_zero_when_nonzero_devices) {
+	assert_bind_assigns_devices_impl_when_nonzero_devices(0);
 }
 
-START_TEST(ar_asio_bind_assigns_devices_impl_to_device_type_one_when_nonzero_devices) {
-	assert_asio_bind_assigns_devices_impl_when_nonzero_devices(1);
+START_TEST(bind_assigns_devices_impl_to_device_type_one_when_nonzero_devices) {
+	assert_bind_assigns_devices_impl_when_nonzero_devices(1);
 }
 
-START_TEST(ar_asio_bind_assigns_device_name_impl_to_device_type_zero_when_nonzero_devices) {
-	set_nonzero_devices();
-	ar_asio_bind_with_device_type(0);
+START_TEST(bind_assigns_device_name_impl_to_device_type_zero_when_nonzero_devices) {
+	bind_nonzero_devices_with_device_type(0);
 	ASSERT_EQUAL_ANY(ar_asio_device_name_stub, _ardvt[0].dev_name);
 }
 
@@ -84,10 +87,10 @@ Suite* arsc_asio_test_suite() {
 	Suite* suite = suite_create("arsc_asio");
 	TCase* test_case = tcase_create("idk");
 	tcase_add_checked_fixture(test_case, setup, teardown);
-	add_test(test_case, ar_asio_bind_returns_number_of_devices);
-	add_test(test_case, ar_asio_bind_assigns_devices_impl_to_device_type_zero_when_nonzero_devices);
-	add_test(test_case, ar_asio_bind_assigns_devices_impl_to_device_type_one_when_nonzero_devices);
-	add_test(test_case, ar_asio_bind_assigns_device_name_impl_to_device_type_zero_when_nonzero_devices);
+	add_test(test_case, bind_returns_number_of_devices);
+	add_test(test_case, bind_assigns_devices_impl_to_device_type_zero_when_nonzero_devices);
+	add_test(test_case, bind_assigns_devices_impl_to_device_type_one_when_nonzero_devices);
+	add_test(test_case, bind_assigns_device_name_impl_to_device_type_zero_when_nonzero_devices);
 	suite_add_tcase(suite, test_case);
 	return suite;
 }
